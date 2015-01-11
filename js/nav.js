b039880d606a0d2e7fd0e803da3b27588c5a5930
Jared Lebel <jared.lebel87@gmail.com>
2015-01-10 20:15:59 -0500

    Added animation.
    Added an animation to cause seizures.

--- js/nav.js
+++ js/nav.js
@@ -2,10 +2,5 @@
 var icon = document.querySelector('.icon');
 	
 	document.querySelector('.icon-btn').addEventListener('click', function(e){
-		e.preventDefault();	
-		if (icon.getAttribute('data-state') == 'expanded'){	
-			icon.setAttribute('data-state', 'collapsed');
-		}else{
-			icon.setAttribute('data-state', 'expanded');
-		}
+		.addClass('.bg-color')
 	});
